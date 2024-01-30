ea88635e89384c6d46c3a47f6586b29006d6583a
Jacob <jacob.benson.dev@gmail.com>
2024-01-30 16:06:58 -0600

    Refactor upload button to separate component
    Abstracted the upload button logic from the GalleryPage into its own component, UploadButton, to improve modularity and code reusability. Relocated client-side directive to the new component file. This change simplifies the GalleryPage's markup and allows for easier maintenance and potential reuse of the upload functionality in different parts of the application.

---
+++ src/app/gallery/upload-button.tsx
@@ -0,0 +1,34 @@
+"use client";
+
+import { Button } from "@/components/ui/button";
+import { CldUploadButton } from "next-cloudinary";
+import { UploadResult } from "../page";
+
+export default function UploadButton() {
+  return (
+    <Button asChild>
+      <div className="flex gap-2">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 24 24"
+          strokeWidth={1.5}
+          stroke="currentColor"
+          className="w-6 h-6">
+          <path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            d="M3 16.5v2.25A2.25 2.25 0 0 0 5.25 21h13.5A2.25 2.25 0 0 0 21 18.75V16.5m-13.5-9L12 3m0 0 4.5 4.5M12 3v13.5"
+          />
+        </svg>
+
+        <CldUploadButton
+          onUpload={(result: UploadResult) => {
+            // setImageId(result.info.public_id);
+          }}
+          uploadPreset="rppmznfn"
+        />
+      </div>
+    </Button>
+  );
+}
