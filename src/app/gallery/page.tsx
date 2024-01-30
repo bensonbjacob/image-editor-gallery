87d31a2708236bb27587df607712a168a580891a
Jacob <jacob.benson.dev@gmail.com>
2024-01-30 16:37:19 -0600

    Integrate Cloudinary for image handling
    Implemented Cloudinary library for better image uploading and management in the gallery module. Added necessary dependencies to package.json and package-lock.json, ensuring compatibility with node version >=9 for the Cloudinary package. Created new CloudinaryImage component to facilitate image rendering with support from the cloud service.
    
    With these changes, image handling within the application is expected to be more efficient and scalable, leveraging Cloudinary's robust capabilities for improved performance and user experience.
    
    Refactors GalleryPage to use the Cloudinary API for fetching and displaying images sorted by the latest uploads, showcasing a grid of images with a predefined size.
    
    This enhancement contributes to a more dynamic and responsive gallery feature, aligning with our goals for a user-friendly media-centric application.

--- src/app/gallery/page.tsx
+++ src/app/gallery/page.tsx
@@ -1,14 +1,36 @@
-import { CldUploadButton } from "next-cloudinary";
-import { UploadResult } from "../page";
-import { Button } from "@/components/ui/button";
+import { Cloud } from "lucide-react";
 import UploadButton from "./upload-button";
+import cloudinary from "cloudinary";
+import CloudinaryImage from "./cloudinary-image";
 
-export default function GalleryPage() {
+type SearchResult = {
+  public_id: string;
+};
+
+export default async function GalleryPage() {
+  const results = (await cloudinary.v2.search
+    .expression("resource_type:image")
+    .sort_by("public_id", "desc")
+    .max_results(5)
+    .execute()) as { resources: SearchResult[] };
+
   return (
     <section>
       <div className="flex justify-between">
         <h1 className="text-4xl font-bold">Gallery</h1>
         <UploadButton />
+
+        <div className="grid grid-cols-4 gap-4">
+          {results.resources.map((result) => (
+            <CloudinaryImage
+              key={result.public_id}
+              src={result.public_id}
+              width="400"
+              height="300"
+              alt="Description of my image"
+            />
+          ))}
+        </div>
       </div>
     </section>
   );
