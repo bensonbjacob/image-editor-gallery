4596310e4701956f1dd5a090a738ecbfca88f735
Jacob <jacob.benson.dev@gmail.com>
2024-02-03 20:28:54 -0600

    Refactor gallery layout to use columns
    Improved image organization within the gallery page by introducing a new column-based layout. This update structures the images into four consistent columns, ensuring a more visually balanced display and enhancing the overall aesthetics of the gallery. Each column now filters and displays a subset of images, providing a cleaner and more scalable UI. No functionality related to image uploading or display has been affected by this refinement.

--- src/app/gallery/page.tsx
+++ src/app/gallery/page.tsx
@@ -1,6 +1,7 @@
 import UploadButton from "./upload-button";
 import cloudinary from "cloudinary";
 import CloudinaryImage from "../../components/cloudinary-image";
+import { get } from "http";
 
 export type SearchResult = {
   public_id: string;
@@ -15,6 +16,14 @@ export default async function GalleryPage() {
     .max_results(30)
     .execute()) as { resources: SearchResult[] };
 
+  const MAX_COLUMNS = 4;
+
+  function getColumns(colIndex: number) {
+    return results.resources.filter(
+      (resource, idx) => idx % MAX_COLUMNS === colIndex
+    );
+  }
+
   return (
     <section>
       <div className="flex flex-col gap-8">
@@ -24,14 +33,23 @@ export default async function GalleryPage() {
         </div>
 
         <div className="grid grid-cols-4 gap-4">
-          {results.resources.map((result) => (
-            <CloudinaryImage
-              key={result.public_id}
-              imageData={result}
-              width="400"
-              height="300"
-              alt="Description of my image"
-            />
+          {[
+            getColumns(0),
+            getColumns(1),
+            getColumns(2),
+            getColumns(3),
+          ].map((column, idx) => (
+            <div key={idx} className="flex flex-col gap-4">
+              {column.map((result) => (
+                <CloudinaryImage
+                  key={result.public_id}
+                  imageData={result}
+                  width="400"
+                  height="300"
+                  alt="Description of my image"
+                />
+              ))}
+            </div>
           ))}
         </div>
       </div>
