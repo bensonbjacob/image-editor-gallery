f73512c682112255afa01beddd70215b6911ad2b
Jacob <jacob.benson.dev@gmail.com>
2024-02-06 20:13:55 -0600

    Create Albums page that will eventually list all albums

---
+++ src/app/albums/page.tsx
@@ -0,0 +1,25 @@
+import cloudinary from "cloudinary";
+
+export type SearchResult = {
+  public_id: string;
+  tags: string[];
+};
+
+export default async function AlbumsPage() {
+  const results = (await cloudinary.v2.search
+    .expression("resource_type:image")
+    .sort_by("created_at", "desc")
+    .with_field("tags")
+    .max_results(30)
+    .execute()) as { resources: SearchResult[] };
+
+  return (
+    <section>
+      <div className="flex flex-col gap-8">
+        <div className="flex justify-between">
+          <h1 className="text-4xl font-bold">Albums</h1>
+        </div>
+      </div>
+    </section>
+  );
+}
