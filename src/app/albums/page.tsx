dc492c94e79135109dfe692bba159d837559ce94
Jacob <jacob.benson.dev@gmail.com>
2024-02-07 19:12:53 -0600

    Introduce AlbumCard component and UI card system
    Implemented a new `AlbumCard` component to display album information and integrated it into the albums page. Developed a set of reusable card UI components to standardize the look and feel across different parts of the application. These changes enhance user experience by presenting albums in a more visually appealing, grid-based layout and promote consistency with a shared card design.

--- src/app/albums/page.tsx
+++ src/app/albums/page.tsx
@@ -1,4 +1,5 @@
 import cloudinary from "cloudinary";
+import { AlbumCard } from "./album-card";
 
 export type Folder = { name: string; path: string };
 
@@ -12,6 +13,11 @@ export default async function AlbumsPage() {
       <div className="flex flex-col gap-8">
         <div className="flex justify-between">
           <h1 className="text-4xl font-bold">Albums</h1>
+        </div>
+        <div className="grid grid-cols-3 gap-4">
+          {folders.map((folder) => (
+            <AlbumCard key={folder.path} folder={folder} />
+          ))}
         </div>
       </div>
     </section>
